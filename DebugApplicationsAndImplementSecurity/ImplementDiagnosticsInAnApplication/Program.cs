0245b75367d49167742e177a40337fcd83d1c85f
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2016-04-24 01:46:27 -0500

    Add solution for objective 3

---
+++ DebugApplicationsAndImplementSecurity/ImplementDiagnosticsInAnApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ImplementDiagnosticsInAnApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Writing data to the event log
+
+            if (!EventLog.SourceExists("MySource"))
+            {
+                EventLog.CreateEventSource("MySource", "MyNewLog");
+                Console.WriteLine("CreatedEventSource");
+                Console.WriteLine("Please restart application");
+                Console.ReadKey();
+                return;
+            }
+
+            EventLog myLog = new EventLog();
+            myLog.Source = "MySource";
+            myLog.WriteEntry("Log event!");
+
+            #endregion
+        }
+    }
+}
